ac73c11c0e2555ed0d52956bb9e543c68a71c121
ctrl-cheeb-del <theredxer@gmail.com>
2023-10-11 14:35:23 +0100

    first commit

---
+++ src/main/java/net/mineshock/sellguifix/ItemParser.java
@@ -0,0 +1,17 @@
+package net.mineshock.sellguifix;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Item;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+public class ItemParser {
+
+//    public static ItemStack parseItem(List<String> itemStringList){
+//        ItemStack
+//    }
+
+
+
+}
