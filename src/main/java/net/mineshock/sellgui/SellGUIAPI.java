d574fd27f8d227421d8ad3eee8ab0d828db448a3
Harry Jones <harrrru2005@gmail.com>
2023-10-20 17:58:08 +0100

    more stuff

--- src/main/java/net/mineshock/sellgui/SellGUIAPI.java
+++ src/main/java/net/mineshock/sellgui/SellGUIAPI.java
@@ -28,76 +28,58 @@ public class SellGUIAPI {
      * @param player    player Player's who's permissions should be evaluated, nullable
      * @return The double value of the ItemStack provided, ItemStack amount is not evaluated or calculated
      */
-    public double getPrice(ItemStack itemStack, @Nullable Player player) {
+    public double getPrice(ItemStack itemStack, Player player) {
         double price = 0.0D;
-        if (!main.getConfig().getBoolean("sell-all-command-sell-enchanted") && itemStack.getEnchantments().size() > 0) {
+        if(!main.getConfig().getBoolean("sell-all-command-sell-enchanted") && itemStack.getEnchantments().size() > 0){
             return price;
         }
-        if (CustomItemsCommand.getPrice(itemStack) != -1.0D)
-            return CustomItemsCommand.getPrice(itemStack);
-        ArrayList<String> flatBonus = new ArrayList<>();
-        if (this.main.getItemPricesConfig().getStringList("flat-enchantment-bonus") != null)
-            for (String s : this.main.getItemPricesConfig().getStringList("flat-enchantment-bonus"))
-                flatBonus.add(s);
-        ArrayList<String> multiplierBonus = new ArrayList<>();
-        if (this.main.getItemPricesConfig().getStringList("multiplier-enchantment-bonus") != null)
-            for (String s : this.main.getItemPricesConfig().getStringList("multiplier-enchantment-bonus"))
-                multiplierBonus.add(s);
-        if (this.main.hasEssentials() && main.getConfig().getBoolean("use-essentials-price")) {
-            if (main.getEssentialsHolder().getEssentials() != null) {
-                if (main.getConfig().getBoolean("use-permission-bonuses-on-essentials")) {
-                    double temp = round(main.getEssentialsHolder().getPrice(itemStack).doubleValue(), main.getConfig().getInt("places-to-round"));
-                    if (player != null) {
-                        for (PermissionAttachmentInfo pai : player.getEffectivePermissions()) {
-                            if (pai.getPermission().contains("sellgui.bonus.") && pai.getValue()) {
-                                if (temp != 0) {
-                                    temp += Double.parseDouble(pai.getPermission().replaceAll("sellgui.bonus.", ""));
-                                }
-                            } else if (pai.getPermission().contains("sellgui.multiplier.") && pai.getValue()) {
-                                temp *= Double.parseDouble(pai.getPermission().replaceAll("sellgui.multiplier.", ""));
-                            }
-                        }
+        if (CustomItemsCommand.getPrice(itemStack) != -1.0D) {
+            price = CustomItemsCommand.getPrice(itemStack);
+        }
+        else {
+            ArrayList<String> flatBonus = new ArrayList<>();
+            if (this.main.getItemPricesConfig().getStringList("flat-enchantment-bonus") != null)
+                for (String s : this.main.getItemPricesConfig().getStringList("flat-enchantment-bonus"))
+                    flatBonus.add(s);
+            ArrayList<String> multiplierBonus = new ArrayList<>();
+            if (this.main.getItemPricesConfig().getStringList("multiplier-enchantment-bonus") != null)
+                for (String s : this.main.getItemPricesConfig().getStringList("multiplier-enchantment-bonus"))
+                    multiplierBonus.add(s);
+            if (this.main.hasEssentials() && main.getConfig().getBoolean("use-essentials-price")) {
+                if (main.getEssentialsHolder().getEssentials() != null) {
+                    return round(main.getEssentialsHolder().getPrice(itemStack).doubleValue(), 3);
+                }
+            }
+            if (itemStack != null && !(itemStack.getType() == Material.AIR) &&
+                    this.main.getItemPricesConfig().contains(itemStack.getType().name()))
+                price = this.main.getItemPricesConfig().getDouble(itemStack.getType().name());
+            if (itemStack != null && itemStack.getItemMeta().hasEnchants()) {
+                for (Enchantment enchantment : itemStack.getItemMeta().getEnchants().keySet()) {
+                    for (String s : flatBonus) {
+                        String[] temp = s.split(":");
+                        if (temp[0].equalsIgnoreCase(enchantment.getKey().getKey()) && temp[1]
+                                .equalsIgnoreCase(itemStack.getEnchantmentLevel(enchantment) + ""))
+                            price += Double.parseDouble(temp[2]);
                     }
-                    if (main.getConfig().getBoolean("round-places")) {
-                        return round(temp, main.getConfig().getInt("places-to-round"));
-                    } else {
-                        return temp;
+                }
+                for (Enchantment enchantment : itemStack.getItemMeta().getEnchants().keySet()) {
+                    for (String s : multiplierBonus) {
+                        String[] temp2 = s.split(":");
+                        if (temp2[0].equalsIgnoreCase(enchantment.getKey().getKey()) && temp2[1]
+                                .equalsIgnoreCase(itemStack.getEnchantmentLevel(enchantment) + ""))
+                            price *= Double.parseDouble(temp2[2]);
                     }
-                } else {
-                    return round(main.getEssentialsHolder().getPrice(itemStack).doubleValue(), main.getConfig().getInt("places-to-round"));
                 }
             }
         }
-        if (itemStack != null && !(itemStack.getType() == Material.AIR) && this.main.getItemPricesConfig().contains(itemStack.getType().name())) {
-            price = this.main.getItemPricesConfig().getDouble(itemStack.getType().name());
-        }
-        if (itemStack != null && itemStack.getItemMeta().hasEnchants()) {
-            for (Enchantment enchantment : itemStack.getItemMeta().getEnchants().keySet()) {
-                for (String s : flatBonus) {
-                    String[] temp = s.split(":");
-                    if (temp[0].equalsIgnoreCase(enchantment.getKey().getKey()) && temp[1]
-                            .equalsIgnoreCase(itemStack.getEnchantmentLevel(enchantment) + ""))
-                        price += Double.parseDouble(temp[2]);
+
+        for(PermissionAttachmentInfo pai : player.getEffectivePermissions()){
+            if(pai.getPermission().contains("sellgui.bonus.")){
+                if(price != 0){
+                    price += Double.parseDouble(pai.getPermission().replaceAll("sellgui.bonus.", ""));
                 }
-            }
-            for (Enchantment enchantment : itemStack.getItemMeta().getEnchants().keySet()) {
-                for (String s : multiplierBonus) {
-                    String[] temp2 = s.split(":");
-                    if (temp2[0].equalsIgnoreCase(enchantment.getKey().getKey()) && temp2[1]
-                            .equalsIgnoreCase(itemStack.getEnchantmentLevel(enchantment) + ""))
-                        price *= Double.parseDouble(temp2[2]);
-                }
-            }
-        }
-        if (player != null) {
-            for (PermissionAttachmentInfo pai : player.getEffectivePermissions()) {
-                if (pai.getPermission().contains("sellgui.bonus.")) {
-                    if (price != 0) {
-                        price += Double.parseDouble(pai.getPermission().replaceAll("sellgui.bonus.", ""));
-                    }
-                } else if (pai.getPermission().contains("sellgui.multiplier.")) {
-                    price *= Double.parseDouble(pai.getPermission().replaceAll("sellgui.multiplier.", ""));
-                }
+            }else if(pai.getPermission().contains("sellgui.multiplier.")){
+                price *= Double.parseDouble(pai.getPermission().replaceAll("sellgui.multiplier.",""));
             }
         }
         return round(price, 3);
